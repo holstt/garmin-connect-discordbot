537ecbec90945ea30e58a6aefe7bdd39008a89fc
holstt <45661813+holstt@users.noreply.github.com>
2023-10-06 20:11:47 +0200

    Refactor discord client

--- src/infra/discord/discord_api_client.py
+++ src/infra/discord/discord_api_client.py
@@ -1,6 +1,5 @@
 import logging
 from io import BytesIO
-from typing import Optional
 
 from discord_webhook import DiscordEmbed, DiscordWebhook
 
@@ -32,13 +31,29 @@ class DiscordApiClient:
 
         self._time_provider = time_provider
 
-    # Generic method for sending an embedded message to Discord
+    def send_message_str(self, message: str) -> None:
+        self._base_client.set_content(message)
+        self._base_client.execute()
+
     def send_message(self, embed: DiscordEmbed) -> None:
         self._base_client.add_embed(embed)
+        self._execute()
 
-        # Always add time to footer
+        # Add time to footer?
         # embed.set_footer(text=f"{self._time_provider.get_current_time()}")
 
+    # XXX: Rate limit on sending multiple images in a row using this method? -> For multi image use send_images
+    def send_image(self, image: BytesIO, name: str) -> None:
+        self._base_client.add_file(file=image.read(), filename=name)
+        self._execute()
+
+    # Attach multiple images to a single message
+    def send_images(self, images: list[BytesIO], names: list[str]) -> None:
+        for image, name in zip(images, names):
+            self._base_client.add_file(file=image.read(), filename=name)
+
+    # Executes current state of the client and resets it
+    def _execute(self) -> None:
         try:
             response = self._base_client.execute(remove_embeds=True)
             response.raise_for_status()
@@ -46,28 +61,3 @@ class DiscordApiClient:
             raise DiscordException(f"Error sending message to Discord: {e}") from e
 
         logger.info(f"Message successfully sent to Discord. Response: {response}")
-
-    # XXX: Rate limit on sending multiple images in a row using this method? -> For multi image use send_images
-    def send_image(self, image: BytesIO, name: str) -> None:
-        self._base_client.add_file(file=image.read(), filename=name)
-
-        try:
-            response = self._base_client.execute()
-            response.raise_for_status()
-        except Exception as e:
-            raise DiscordException(f"Error sending image to Discord: {e}") from e
-
-        logger.info(f"Image successfully sent to Discord. Response: {response}")
-
-    # Attach multiple images to a single message
-    def send_images(self, images: list[BytesIO], names: list[str]) -> None:
-        for image, name in zip(images, names):
-            self._base_client.add_file(file=image.read(), filename=name)
-
-        try:
-            response = self._base_client.execute()
-            response.raise_for_status()
-        except Exception as e:
-            raise DiscordException(f"Error sending images to Discord: {e}") from e
-
-        logger.info(f"Images successfully sent to Discord. Response: {response}")
