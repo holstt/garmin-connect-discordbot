8bda5cff850aa569c6da3733d9e146891b6ab950
holstt <45661813+holstt@users.noreply.github.com>
2023-12-30 22:54:25 +0100

    feat: add custom exception on input prompt exceptions

--- src/presentation/login_prompt.py
+++ src/presentation/login_prompt.py
@@ -6,9 +6,19 @@ logger = logging.getLogger(__name__)
 # Prompts the user for login credentials
 
 
+class InputException(Exception):
+    pass
+
+
 def prompt_email():
     logger.info("Prompting for email")
-    email: str = input("Email: ")
+
+    try:
+        email: str = input("Email: ")
+    except Exception as e:
+        raise InputException(
+            f"Exception occured while trying to get email input from user: {e}"
+        ) from e
     if not email:
         raise ValueError("Email must be provided")
     return email
@@ -16,7 +26,14 @@ def prompt_email():
 
 def prompt_password():
     logger.info("Prompting for password")
-    password = getpass.getpass("Password: ")
+
+    try:
+        password = getpass.getpass("Password: ")
+    except Exception as e:
+        raise InputException(
+            f"Exception occured while trying to get password input from user: {e}"
+        ) from e
+
     if not password:
         raise ValueError("Password must be provided")
     return password
