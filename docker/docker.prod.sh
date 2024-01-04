a9c34b43d306a3a737f5f02d05247103b3d99980
holstt <45661813+holstt@users.noreply.github.com>
2024-01-04 23:32:43 +0100

    build(docker): refactor docker helper script

--- docker/docker.prod.sh
+++ docker/docker.prod.sh
@@ -6,13 +6,12 @@ echo "Running script: $0"
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-# Location of .env and the creation of the container data directory is assumed to be placed directly in the project root directory
-CONTAINER_DATA_DIR="$SCRIPT_DIR/../data"
-ENV_PATH="$SCRIPT_DIR/../.env.prod"
-
 # User that will be created (if not exist) and used for running the container
 USERNAME="garmin-connect-bot"
-GROUPNAME=$USERNAME
+# Location of .env and the creation of the container data directory is assumed to be placed directly in the project root directory
+CONTAINER_DATA_DIR="$SCRIPT_DIR/../data"
+ENV_PATH="$SCRIPT_DIR/../prod.env"
+# Command to run the project
+COMMAND="DOCKER_REPO_NAME=my-docker-repo docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d"
 
-"$SCRIPT_DIR"/docker_setup.sh "$CONTAINER_DATA_DIR" "$ENV_PATH" $USERNAME $GROUPNAME
-"$SCRIPT_DIR"/docker_run.sh "$CONTAINER_DATA_DIR" "$ENV_PATH" $USERNAME $GROUPNAME
+"$SCRIPT_DIR"/docker_setup.sh "$USERNAME" "$CONTAINER_DATA_DIR" "$ENV_PATH" "$COMMAND"
--- docker/docker_setup.sh
+++ docker/docker_setup.sh
@@ -16,14 +16,14 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # Get args
-CONTAINER_DATA_DIR=$1
-ENV_PATH=$2
-USERNAME=$3
-GROUPNAME=$4
+USERNAME=$1
+CONTAINER_DATA_DIR=$2
+ENV_PATH=$3
+COMMAND=$4
 
 # Ensure arguments are provided
-if [ -z "$CONTAINER_DATA_DIR" ] || [ -z "$ENV_PATH" ] || [ -z "$USERNAME" ] || [ -z "$GROUPNAME" ]; then
-  echo "Error: Missing arguments. Usage: ./docker.sh <container_data_dir> <env_path> <username> <groupname>"
+if [ -z "$USERNAME" ] || [ -z "$CONTAINER_DATA_DIR" ] || [ -z "$ENV_PATH" ] || [ -z "$COMMAND" ]; then
+  echo "Error: Missing arguments. Usage: ./docker_setup.sh <username> <container_data_dir> <env_path> <command>"
   exit 1
 fi
 
@@ -33,18 +33,25 @@ echo "Docker setup started"
 sudo adduser --system --disabled-login --disabled-password --no-create-home "$USERNAME"
 
 # Create a dedicated group for the container and add the user to it
-sudo groupadd "$GROUPNAME" && echo "Group $GROUPNAME created"
-sudo usermod -aG "$GROUPNAME" "$USERNAME"
+sudo groupadd "$USERNAME" && echo "Group $USERNAME created"
+sudo usermod -aG "$USERNAME" "$USERNAME"
 
 # Set up data directory for the container to store its data on host
 sudo mkdir -p "$CONTAINER_DATA_DIR" && echo "Ensure created directory: $CONTAINER_DATA_DIR"
-sudo chown "$USERNAME":"$GROUPNAME" "$CONTAINER_DATA_DIR"
+sudo chown "$USERNAME":"$USERNAME" "$CONTAINER_DATA_DIR"
 # Restrict access to the data directory (all permissions to user only), such that we can store sensitive data
 sudo chmod 700 "$CONTAINER_DATA_DIR"
 
 # Ensure permission for env path
-sudo chown "$USERNAME":"$GROUPNAME" "$ENV_PATH"
+sudo chown "$USERNAME":"$USERNAME" "$ENV_PATH"
 # Restrict access, readable by user only
 sudo chmod 400 "$ENV_PATH"
 
 echo "Docker setup complete."
+
+# Run the project
+echo "Running docker-compose project"
+cd "$SCRIPT_DIR"
+
+# Run command
+eval "$COMMAND"
